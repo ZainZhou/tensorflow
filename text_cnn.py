38c4a35490ae2641a2ab104a05b4312b8e1b058f
T&H <truemenhale@gmail.com>
2018-08-27 17:30:29 +0800

    cnn

--- text_cnn.py
+++ text_cnn.py
@@ -1,2 +1,4 @@
-def TextCNN():
-    pass
+import tensorflow as tf
+class TextCNN():
+    def __init__(self,sequence_length,num_classes,vocab_size,embedding_size,filter_sizes,num_filters,l2_reg_lamdba):
+        self.input_x = tf.placeholder(tf.int32,[None,sequence_length],name = 'input_x')
--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import os
 import time
 import datetime
 import data_helpers
-# from text_cnn import TextCNN
+from text_cnn import TextCNN
 from tensorflow.contrib import learn
 
 # Params
@@ -12,7 +12,8 @@ from tensorflow.contrib import learn
 tf.flags.DEFINE_float('dev_sample_percentage',1,'percentage of the training data to use for validation')
 tf.flags.DEFINE_string('positive_data_file','./data/rt-polaritydata/rt-polarity.pos','Data source for the positive')
 tf.flags.DEFINE_string('negative_data_file','./data/rt-polaritydata/rt-polarity.neg','Data source for the negative')
-
+tf.flags.DEFINE_boolean('allow_soft_placement',True,'allow_soft_placement')
+tf.flags.DEFINE_boolean('log_device_placement',True,'log_soft_placement')
 # Model Hparams
 tf.flags.DEFINE_integer('embedding',128,'Dimensionality of character embedding')
 tf.flags.DEFINE_string('filter_size','3,4,5','filter size')
@@ -42,3 +43,27 @@ np.random.seed(10)
 shuffle_index = np.random.permutation(np.arange(len(y)))
 x_shuffled = x[shuffle_index]
 y_shuffled = y[shuffle_index]
+
+dev_sample_index = -1*int(FLAGS.dev_sample_percentage*float(len(y)))
+
+x_train,x_dev = x_shuffled[:dev_sample_index],x_shuffled[dev_sample_index:]
+y_train,y_dev = y_shuffled[:dev_sample_index],y_shuffled[dev_sample_index:]
+print(('max_document_length:{:d}').format(len(vocab_processor.vocabulary_)))
+print(('train/dev split:{:d}/{:d}').format(len(y_train),len(y_dev)))
+
+with tf.Graph().as_default():
+    session_conf = tf.ConfigProto(
+        allow_soft_placement = FLAGS.allow_soft_placement,
+        log_device_placement = FLAGS.log_device_placement
+    )
+    sess = tf.Session(config=session_conf)
+    with sess.as_default():
+        cnn = TextCNN(
+            sequence_length = x_train.shape[1],
+            num_classes = y_train.shape[1],
+            vocab_size = len(vocab_processor.vocabulary_),
+            embedding_size = FLAGS.embedding,
+            filter_sizes = list(map(int,FLAGS.filter_size.split(','))),
+            num_filters = FLAGS.num_filters,
+            l2_reg_lamdba = FLAGS.L2_reg_lambda
+        )
